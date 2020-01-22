4405ee7c43c19cee407e77eb0a849ec8244d6a23
Peter Carbonetto <peter.carbonetto@gmail.com>
2020-01-22 11:06:44 -0600

    Added help page for scale_faster.

--- R/fasterscale.R
+++ R/fasterscale.R
@@ -1,4 +1,17 @@
+#' @title Fast Centering and Scaling of Matrices
+#'
+#' @description Does the same thing as \code{scale(x)}, but faster.
+#'
+#' @details Uses \code{\link[matrixStats]{colSds}} from the
+#'   \code{matrixStats} package.
+#'
+#' @param x A numeric matrix; \code{is.matrix(x)} should be \code{TRUE}.
+#'
+#' @return A matrix in which the columns are centered to have zero
+#' mean, and they are also scaled to have standard deviation of 1.
+#' 
 #' @export
+#' 
 scale_faster <- function (x) {
   a <- colMeans(x)
   b <- matrixStats::colSds(x)
